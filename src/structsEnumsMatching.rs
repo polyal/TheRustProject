8d2978b85d0b351181fe1f8797a2c53cd16ae14d
Lorant Polya <lorant.polya@gmail.com>
2021-12-30 22:01:47 -0500

    Added skeleton code for 'Structs, Enums, and Matching'

---
+++ src/structsEnumsMatching.rs
@@ -0,0 +1,7 @@
+// https://stevedonovan.github.io/rust-gentle-intro/readme.html
+// https://stevedonovan.github.io/rust-gentle-intro/2-structs-enums-lifetimes.html
+
+fn main() 
+{
+    println!("Hello Worlds");
+}
